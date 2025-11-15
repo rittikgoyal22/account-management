17a5c48f6663f70de5f92cc69f4315398a34284d
Rittik <rittikgoyal22@gmail.com>
2025-11-16 00:36:13 +0530

    added sonar fix

--- src/main/java/com/etd/account_management/dao/GradeHistoryRepo.java
+++ src/main/java/com/etd/account_management/dao/GradeHistoryRepo.java
@@ -2,7 +2,6 @@ package com.etd.account_management.dao;
 
 import com.etd.account_management.entity.GradeHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
